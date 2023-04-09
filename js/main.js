2a801dc14c1b62d7e74deffa65f25451ae037353
Max <max.kuzn.msx@gmail.com>
2023-04-09 23:19:05 +0300

    desktop v of catalog, about, header and footer

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.home-cat .swiper', {
+
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop: false,
+    slidesPerGroup: 3,
+    // Navigation arrows
+    navigation: {
+        nextEl: '.home-cat__next',
+        prevEl: '.home-cat__prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 0
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 14
+        },
+        // when window width is >= 640px
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 14
+        },
+        // when window width is >= 640px
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 15
+        }
+    }
+});
